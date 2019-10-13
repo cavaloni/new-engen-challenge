6a9b435871e26ac0fcdeca44d9d1c8c1e8580b90
root <root@BrainStem.localdomain>
2019-10-13 15:18:53 -0700

    proper build dir

--- server/index.ts
+++ server/index.ts
@@ -78,7 +78,7 @@ app.post("/api/user/palettes", (req: any, res: any) => {
 });
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/build/index.html"));
+	res.sendFile(path.join(__dirname + "../build/index.html"));
 });
 
 app.listen(process.env.PORT || 3001, () =>
